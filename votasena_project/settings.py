9455fd6a81d8a24166bdaeeb9868448aa09c1577
rodrigocastano2878-lang <rodrigocastano2878@gmail.com>
2026-06-25 22:26:32 -0600

    Update database settings to use MySQL
    Changed database configuration from SQLite to MySQL.

--- votasena_project/settings.py
+++ votasena_project/settings.py
@@ -80,9 +80,12 @@ WSGI_APPLICATION = 'votasena_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'votasena_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
